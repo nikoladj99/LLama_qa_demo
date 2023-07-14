f997ac08c8b9eeb59fda3cd8848a6c0f79ebd834
nikoladj99 <nikoladj@elfak.rs>
2023-07-14 17:23:48 +0200

    Promenjen model na t5-small

--- llama.py
+++ llama.py
@@ -44,7 +44,7 @@ st.write('Current temperature:', temperature_value)
 
 #odabir modela 
 access_token = "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"
-repo_id = "google/flan-t5-base"
+repo_id = "google/flan-t5-small"
 llm_predictor = LLMPredictor(llm = HuggingFaceHub(
     repo_id=repo_id, 
     model_kwargs= {"temperature": temperature_value, "max_length": 64},
